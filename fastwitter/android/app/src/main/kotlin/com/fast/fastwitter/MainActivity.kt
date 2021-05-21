2fb54c971e038ea6c0c2a428350606902c7714ed
samuzaffar99 <sam.muzaffar99@gmail.com>
2021-05-21 15:23:33 +0500

    new project with web support

---
+++ fastwitter/android/app/src/main/kotlin/com/fast/fastwitter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fast.fastwitter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fastwitter/lib/Screens/LoginScreen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:fastwitter/Constants/Constants.dart';
+import 'package:fastwitter/Services/auth_service.dart';
+import 'package:fastwitter/Widgets/RoundedButton.dart';
+
+import 'FeedScreen.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String _email;
+  String _password;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: KTweeterColor,
+        centerTitle: true,
+        elevation: 0,
+        title: Text(
+          'Log in',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter Your email',
+              ),
+              onChanged: (value) {
+                _email = value;
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Enter Your password',
+              ),
+              onChanged: (value) {
+                _password = value;
+              },
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            RoundedButton(
+              btnText: 'LOG IN',
+              onBtnPressed: () async {
+                bool isValid = await AuthService.login(_email, _password);
+                if (isValid) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return FeedScreen(currentUserId: AuthService.signedInUser.uid);
+                      },
+                    ),
+                  );
+                  // AuthService.
+                  // Navigator.pop(context);
+                } else {
+                  print('login problem');
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
